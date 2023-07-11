ce708e98625148bd9eba77de75d8bbe39a1bec53
MishB1 <88148320+MishB1@users.noreply.github.com>
2023-07-11 00:55:22 +0000

    homepage done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(
   MaterialApp(
@@ -18,14 +20,52 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     body: Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image:AssetImage('assets/images/one.jpg'),
-          fit: BoxFit.fill
+      resizeToAvoidBottomInset : false,
+      appBar: AppBar(
+          backgroundColor: Colors.black, systemOverlayStyle: SystemUiOverlayStyle.light,
+          elevation: 0,
+          leading: IconButton(
+            icon: Icon(Icons.menu, color: Colors.white,),
+            onPressed: () {},
+          ),
+        ),
+      
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image:AssetImage('assets/images/three.jpg'),
+              fit: BoxFit.fill
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Stock', style: TextStyle(color: Colors.white, fontSize: 30),) ,
+                  SizedBox(height: 2,),
+                  Text('Predictor', style: TextStyle(color: Colors.white, fontSize: 45, fontWeight: FontWeight.bold),),
+                  SizedBox(height: 20,),
+                  TextField(
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(width: 1.5, color: Colors.white),
+                        borderRadius: BorderRadius.circular(50.0),
+                      ),
+                      prefixIcon:   Icon(Icons.search, color: Colors.white,),
+                      hintText: "Search your stock here...",
+                      hintStyle: TextStyle(color: Colors.white, fontSize: 15)
+                    ),
+                  ),
+                ] 
+            ),
+           )
+          ],
         ),
       ),
-     ),
     );  
   }
   
