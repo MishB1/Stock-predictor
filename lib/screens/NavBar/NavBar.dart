3d31569f005bfa6a9bfaecea5f59e10452fc3683
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-08-20 02:23:19 +0000

    add: bottom navigation bar added and implemented into workflow

--- lib/screens/NavBar/NavBar.dart
+++ lib/screens/NavBar/NavBar.dart
@@ -1,5 +1,5 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:stock_predictor/screens/News/news.dart';
 import 'package:stock_predictor/screens/Notificattions/notifications.dart';
 import 'package:stock_predictor/screens/SettingsPage/settings.dart';
@@ -7,127 +7,152 @@ import 'package:stock_predictor/screens/UpdatesPage/updates.dart';
 import 'package:stock_predictor/screens/about/aboutScreen.dart';
 import 'package:share/share.dart';
 
-class NavBar extends StatelessWidget {
-  const NavBar({super.key});
-
-  final String shareText = 'Check out the Stock Prediction App! 📈🚀';
-
-  void signOut() {
-    FirebaseAuth.instance.signOut();
-  }
+class BottomNavBar extends StatelessWidget {
+  final int currentIndex;
+  final void Function(int) onTap;
+  final List<BottomNavigationBarItem> items;
+  const BottomNavBar(
+      {super.key,
+      required this.items,
+      required this.currentIndex,
+      required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return  Drawer(
-      child: ListView(        
-        padding: EdgeInsets.zero,        
-        children: [
-          UserAccountsDrawerHeader(
-            accountName: Text('Hamish Blay'), 
-            accountEmail: Text('hamishblay@gmail.com'),
-            currentAccountPicture: CircleAvatar(
-              child: ClipOval(
-                child: Image.asset('assets/images/me.jpg', 
-                width: 90,
-                height: 90,
-                fit: BoxFit.cover,),
-              ),
-            ),
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              image: DecorationImage(
-                image: AssetImage('assets/images/four.jpg'),
-                fit: BoxFit.cover
-              )
-            ),
-          ),
-
-          SizedBox(height: 30,),
-          ListTile(
-            leading: Icon(Icons.favorite_outline, color: Colors.lightBlue,),
-            title: Text('Favorites'),
-            onTap: () {},  
-          ),
-
-          SizedBox(height: 10,),
-          ListTile(
-            leading: Icon(Icons.share_outlined, color: Colors.lightBlue,),
-            title: Text('Share with Friends'),
-            onTap: () {
-              Share.share(shareText);
-            },
-          ),
-
-          SizedBox(height: 10,),
-          ListTile(
-            leading: Icon(Icons.newspaper_outlined, color: Colors.lightBlue,),
-            title: Text('News'),
-            onTap: () {
-              Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => NewsScreen()),
-             );
-            },
-          ),
-
-          SizedBox(height: 10,),
-          ListTile(
-            leading: Icon(Icons.notifications_outlined, color: Colors.lightBlue,),
-            title: Text('Notifications'),
-            onTap: () {
-              Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => NotificationsScreen()),
-             );
-            },
-          ),
-
-          SizedBox(height: 10,),
-          ListTile(
-            leading: Icon(Icons.person_outline, color: Colors.lightBlue,),
-            title: Text('About'),
-            onTap: () {
-              Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => aboutScreen()),
-             );
-            },
-          ),
-
-          SizedBox(height: 10,),
-          Divider(),
-          SizedBox(height: 10,),
-          ListTile(
-            leading: Icon(Icons.settings_outlined, color: Colors.lightBlue,),
-            title: Text('Settings'),
-            onTap: () {
-              Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => SettingsPage()),
-             );
-            },
-          ),
-
-          SizedBox(height: 10,),
-          ListTile(
-            leading: Icon(Icons.update_outlined, color: Colors.lightBlue,),
-            title: Text('Updates'),
-            onTap: () {
-              Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => UpdatesPage()),
-             );
-            },
-          ),
-          
-          SizedBox(height: 10,),
-          ListTile(
-            leading: Icon(Icons.exit_to_app, color: Colors.lightBlue,),
-            title: Text('SignOut'),
-            onTap: () {signOut();},
-          ),
-        ],
-      ),
+    return BottomNavigationBar(
+      onTap: onTap,
+      items: items,
+      enableFeedback: true,
+      showSelectedLabels: false,
+      currentIndex: currentIndex,
+      showUnselectedLabels: false,
+      backgroundColor: Colors.white,
+      selectedItemColor: Colors.black,
     );
   }
 }
+
+// class NavBar extends StatelessWidget {
+//   const NavBar({super.key});
+
+//   final String shareText = 'Check out the Stock Prediction App! 📈🚀';
+
+//   void signOut() {
+//     FirebaseAuth.instance.signOut();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return  Drawer(
+//       child: ListView(        
+//         padding: EdgeInsets.zero,        
+//         children: [
+//           UserAccountsDrawerHeader(
+//             accountName: Text('Hamish Blay'), 
+//             accountEmail: Text('hamishblay@gmail.com'),
+//             currentAccountPicture: CircleAvatar(
+//               child: ClipOval(
+//                 child: Image.asset('assets/images/me.jpg', 
+//                 width: 90,
+//                 height: 90,
+//                 fit: BoxFit.cover,),
+//               ),
+//             ),
+//             decoration: BoxDecoration(
+//               color: Colors.blue,
+//               image: DecorationImage(
+//                 image: AssetImage('assets/images/four.jpg'),
+//                 fit: BoxFit.cover
+//               )
+//             ),
+//           ),
+
+//           SizedBox(height: 30,),
+//           ListTile(
+//             leading: Icon(Icons.favorite_outline, color: Colors.lightBlue,),
+//             title: Text('Favorites'),
+//             onTap: () {},  
+//           ),
+
+//           SizedBox(height: 10,),
+//           ListTile(
+//             leading: Icon(Icons.share_outlined, color: Colors.lightBlue,),
+//             title: Text('Share with Friends'),
+//             onTap: () {
+//               Share.share(shareText);
+//             },
+//           ),
+
+//           SizedBox(height: 10,),
+//           ListTile(
+//             leading: Icon(Icons.newspaper_outlined, color: Colors.lightBlue,),
+//             title: Text('News'),
+//             onTap: () {
+//               Navigator.push(
+//               context,
+//               MaterialPageRoute(builder: (context) => NewsScreen()),
+//              );
+//             },
+//           ),
+
+//           SizedBox(height: 10,),
+//           ListTile(
+//             leading: Icon(Icons.notifications_outlined, color: Colors.lightBlue,),
+//             title: Text('Notifications'),
+//             onTap: () {
+//               Navigator.push(
+//               context,
+//               MaterialPageRoute(builder: (context) => NotificationsScreen()),
+//              );
+//             },
+//           ),
+
+//           SizedBox(height: 10,),
+//           ListTile(
+//             leading: Icon(Icons.person_outline, color: Colors.lightBlue,),
+//             title: Text('About'),
+//             onTap: () {
+//               Navigator.push(
+//               context,
+//               MaterialPageRoute(builder: (context) => aboutScreen()),
+//              );
+//             },
+//           ),
+
+//           SizedBox(height: 10,),
+//           Divider(),
+//           SizedBox(height: 10,),
+//           ListTile(
+//             leading: Icon(Icons.settings_outlined, color: Colors.lightBlue,),
+//             title: Text('Settings'),
+//             onTap: () {
+//               Navigator.push(
+//               context,
+//               MaterialPageRoute(builder: (context) => SettingsPage()),
+//              );
+//             },
+//           ),
+
+//           SizedBox(height: 10,),
+//           ListTile(
+//             leading: Icon(Icons.update_outlined, color: Colors.lightBlue,),
+//             title: Text('Updates'),
+//             onTap: () {
+//               Navigator.push(
+//               context,
+//               MaterialPageRoute(builder: (context) => UpdatesPage()),
+//              );
+//             },
+//           ),
+          
+//           SizedBox(height: 10,),
+//           ListTile(
+//             leading: Icon(Icons.exit_to_app, color: Colors.lightBlue,),
+//             title: Text('SignOut'),
+//             onTap: () {signOut();},
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
--- lib/screens/homeScreen/homeScreen.dart
+++ lib/screens/homeScreen/homeScreen.dart
@@ -3,6 +3,10 @@ import 'package:stock_predictor/components/favoriteStockWidget.dart';
 import 'package:stock_predictor/models/favorite_stock_model.dart';
 import 'package:stock_predictor/models/news_model.dart';
 import 'package:stock_predictor/news/news.dart';
+import 'package:stock_predictor/screens/MarketTrends/marketTrends.dart';
+import 'package:stock_predictor/screens/NavBar/NavBar.dart';
+import 'package:stock_predictor/screens/News/news.dart';
+import 'package:stock_predictor/screens/SettingsPage/settings.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -15,6 +19,9 @@ class HomeScreen extends StatefulWidget {
 
 class _AppState extends State<HomeScreen> {
   final _textController = TextEditingController();
+
+  int currentIndex = 0;
+
   List<News>? news;
   List<Color> colors = [Colors.black, Colors.teal, Colors.blueAccent];
   List<FavoriteStock> favoriteStocks = [
@@ -56,6 +63,32 @@ class _AppState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    List<BottomNavigationBarItem> navBarItems = [
+      BottomNavigationBarItem(
+        label: 'Hot Picks',
+        icon: currentIndex == 0
+            ? Image.asset('assets/images/hot_pick_selected.png')
+            : Image.asset('assets/images/hot_pick_unselected.png'),
+      ),
+      BottomNavigationBarItem(
+        label: 'Home',
+        icon: currentIndex == 1
+            ? Image.asset('assets/images/home_selected.png')
+            : Image.asset('assets/images/home_unselected.png'),
+      ),
+      BottomNavigationBarItem(
+        label: 'Settings',
+        icon: currentIndex == 2
+            ? Image.asset('assets/images/settings_selected.png')
+            : Image.asset('assets/images/settings_unselected.png'),
+      ),
+    ];
+
+    List<Widget> pages = [
+      NewsScreen(),
+      MarketTrends(colors: colors, news: news, favoriteStocks: favoriteStocks),
+      SettingsPage(),
+    ];
     return Scaffold(
       backgroundColor: Colors.white,
       // resizeToAvoidBottomInset: false,
@@ -72,149 +105,15 @@ class _AppState extends State<HomeScreen> {
           ),
         ),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            padding: EdgeInsets.all(20.0),
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              Text(
-                'Trending News',
-                style: TextStyle(
-                  fontSize: 14,
-                  color: Colors.grey.shade400,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              SizedBox(
-                height: 180,
-                child: ListView.builder(
-                  itemCount: 3,
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        width: 300,
-                        padding: EdgeInsets.all(15),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(15),
-                            color: colors[index],
-                            boxShadow: [
-                              BoxShadow(
-                                offset: Offset(0, 3),
-                                color: Colors.black.withOpacity(0.3),
-                                blurRadius: 6,
-                                spreadRadius: 1,
-                              ),
-                            ]),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              news?[index].title ?? '',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Row(
-                              children: [
-                                Text(
-                                  'Source: ',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                Text(
-                                  news?[index].source ?? '',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-
-              SizedBox(
-                height: 40,
-              ),
-              Text(
-                'Favorites',
-                style: TextStyle(
-                  fontSize: 14,
-                  color: Colors.grey.shade400,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-
-              SizedBox(
-                height: 150,
-                child: ListView.builder(
-                  itemCount: favoriteStocks.length,
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (context, index) {
-                    FavoriteStock favoriteStock = favoriteStocks[index];
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: buildFavoriteStock(
-                        stockPrice: favoriteStock.stockPrice,
-                        percentage: favoriteStock.percentage,
-                        stockName: favoriteStock.stockName,
-                        stockImagePath: favoriteStock.stockImagePath,
-                      ),
-                    );
-                  },
-                ),
-              ),
-              // TextField(
-              //   controller: _textController,
-              //   style: TextStyle(color: Colors.white),
-              //   decoration: InputDecoration(
-              //       enabledBorder: OutlineInputBorder(
-              //         borderSide: BorderSide(width: 1.0, color: Colors.white),
-              //         borderRadius: BorderRadius.circular(50.0),
-              //       ),
-              //       prefixIcon: Icon(
-              //         Icons.search,
-              //         color: Colors.white,
-              //       ),
-              //       suffixIcon: IconButton(
-              //           onPressed: () {
-              //             _textController.clear();
-              //           },
-              //           icon: Icon(
-              //             Icons.clear,
-              //             color: Colors.white,
-              //           )),
-              //       hintText: "Search your stock here...",
-              //       hintStyle: TextStyle(color: Colors.white, fontSize: 15)),
-              // ),
-            ]),
-          )
-        ],
-      ),
+      body: pages.elementAt(currentIndex),
+      bottomNavigationBar: BottomNavBar(
+          items: navBarItems,
+          currentIndex: currentIndex,
+          onTap: (value) {
+            setState(() {
+              currentIndex = value;
+            });
+          }),
     );
   }
 }
